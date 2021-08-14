9c56e7ef496d4a4e6fe6413c31dd0c507b3dab5d
Mostafa S. El-bastawisy <84564484+Mostafa-S-El-Bastawisy@users.noreply.github.com>
2021-08-14 19:35:54 +0200

    End tasks

--- main.js
+++ main.js
@@ -57,18 +57,81 @@ document.write( showDetails( false, "Osama", 38 ) ); // "Hello Osama, Your Age I
 
 //-----------------------------------//
 
+	// if(theGender === "Male"){
+	// 	console.log(`hello Mr ${theName}`);
+	// }if(theGender === "Female"){
+	// 	console.log(`hello Miss ${theName}`);
+	// }else{
+	// 	console.log(`hello ${theName}`)
+	// }
 function sayHello(theName, theGender) {
+	
 	if(theGender === "Male"){
-		console.log(`hello Mr ${theName}`);
-	}if(theGender === "Female"){
-		console.log(`hello Miss ${theName}`);
-	}else{
-		console.log(`hello ${theName}`)
+		console.log(`Hello Mr ${theName}`);
+	}else if(theGender === "Female"){
+		console.log(`Hello Miss ${theName}`);
+	}else {
+		console.log(`Hello ${theName}`);
 	}
+	
 }
 
 // Needed Output
 sayHello("Osama", "Male"); // "Hello Mr Osama"
 sayHello("Eman", "Female"); // "Hello Miss Eman"
 sayHello("Sameh"); // "Hello Sameh"
+
+console.log("=========================");
+
+function calculate(firstNum, secondNum = " Second Number Not Found", operation) {
+  // Your Code Here
+
+if(firstNum && secondNum && operation === "add"){
+	console.log(firstNum + secondNum);
+}else if(firstNum && secondNum && operation === "subtract"){
+	console.log(firstNum - secondNum);
+}else if(firstNum && secondNum && operation === "multiply"){
+	console.log(firstNum * secondNum);
+}else{
+	console.log(firstNum + secondNum);
+}
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
+
+console.log("=========================");
+
+function ageInTime(theAge) {
+if (theAge < 10 || theAge > 100) {
+		console.log("Age Out Of Range") ;
+	}else{
+		let months = theAge * 12,
+		weeks = months * 4,
+		days = weeks * 7,
+		hours = days * 24,
+		minutes = hours * 60,
+		seconds = minutes * 60;
+	console.log(`${months} Months\n${weeks} Weeks\n${hours} Hours\n${minutes} Minutes\n${seconds} Seconds`) ;
+	}
+}
+// Needed Output
+ageInTime(110); // Age Out Of Range
+ageInTime(38); // Months Example => 456 Months
+console.log("=========================");
+
+function createSelectBox(startYear, endYear) {
+	document.write("<hr><select>");
+	for (let i = startYear; i <= endYear; i++) {
+		document.write(`<option value="${i}">${i}</option>`);
+	}
+	document.write("</select>");
+}
+createSelectBox(2000, 2021);
+console.log("=========================");
+
 
